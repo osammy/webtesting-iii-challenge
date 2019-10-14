bfebd9dfae4c47a77eb1d5b2507ae3aa0ecd4a46
samuel <osamaimafidon@gmail.com>
2019-10-14 19:37:01 +0100

    mvp complete

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,42 @@
 // Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+afterEach(rtl.cleanup);
+let wrapper;
+beforeEach(() => {
+    wrapper = rtl.render(<Dashboard />);
+});
+
+describe('Gate', () => {
+    it('its `open` at initial render', () => {
+        expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+    });
+    it('its `unlocked` at initial render', () => {
+        expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+    });
+    it('cannot toggle closed or open if locked', () => {
+        expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+        rtl.fireEvent.click(wrapper.queryByText(/close gate/i));
+        expect(wrapper.queryByText(/closed/i)).toBeInTheDocument();
+        rtl.fireEvent.click(wrapper.queryByText(/lock gate/i));
+        expect(wrapper.queryByText(/locked/i)).toBeInTheDocument();
+        rtl.fireEvent.click(wrapper.queryByText(/open gate/i));
+        expect(wrapper.queryByText(/closed/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/locked/i)).toBeInTheDocument();
+    });
+});
+
+describe('Dashboard component', () => {
+    it('Displays Controls', () => {
+        expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
+    });
+    it('Shows display', () => {
+        expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+    });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,54 @@
 // Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "./Display";
+
+afterEach(rtl.cleanup);
+
+describe("Display component", () => {
+  it("Displays if gate is closed", () => {
+    const wrapper = rtl.render(<Display closed={true} />);
+    expect(wrapper.queryByText(/open/i)).not.toBeInTheDocument();
+    expect(wrapper.queryByText(/closed/i)).toBeInTheDocument();
+  });
+  it("Displays if gate is unlocked", () => {
+    const wrapper = rtl.render(<Display />);
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+  });
+  it("Uses the red-led class when locked", () => {
+    const wrapper = rtl.render(<Display locked={true} />);
+    expect(wrapper.queryByText(/locked/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/locked/i)).toHaveClass("red-led");
+    expect(wrapper.queryByText(/locked/i)).not.toHaveClass("green-led");
+  });
+  it("Uses the green-led class when open", () => {
+    const wrapper = rtl.render(<Display />);
+    expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/open/i)).toHaveClass("green-led");
+    expect(wrapper.queryByText(/open/i)).not.toHaveClass("red-led");
+  });
+  it("Displays if gate is open", () => {
+    const wrapper = rtl.render(<Display />);
+    expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/closed/i)).not.toBeInTheDocument();
+  });
+  it("Displays if gate is locked", () => {
+    const wrapper = rtl.render(<Display locked={true} />);
+    expect(wrapper.queryByText(/locked/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/unlocked/i)).not.toBeInTheDocument();
+  });
+  it("Uses the red-led class when closed", () => {
+    const wrapper = rtl.render(<Display closed={true} />);
+    expect(wrapper.queryByText(/closed/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/closed/i)).toHaveClass("red-led");
+    expect(wrapper.queryByText(/closed/i)).not.toHaveClass("green-led");
+  });
+
+  it("Uses the green-led class when unlocked", () => {
+    const wrapper = rtl.render(<Display />);
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/unlocked/i)).toHaveClass("green-led");
+    expect(wrapper.queryByText(/unlocked/i)).not.toHaveClass("red-led");
+  });
+});
